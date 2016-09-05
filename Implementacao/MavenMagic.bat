e5b598e1dd0e27afe1032a21bb179383ae0bb581
marcelo.lima <marcelo.lima@PJMRS12.redempm.mpu>
2016-09-05 10:46:46 -0300

    Banco de dados: o campo email da tabela Usuario deve ser unique

--- Implementacao/MavenMagic.bat
+++
@@ -1,3 +0,0 @@
-set PATH=%PATH%;C:\apache-maven-3.3.9\bin
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_71
-mvn archetype:generate -DgroupId=br.com.witc -DartifactId=WritingInTheClouds -DarchetypeArtifactId=maven-archetype-webapp -DinteractiveMode=false
