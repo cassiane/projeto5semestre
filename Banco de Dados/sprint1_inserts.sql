4525f409bc9ef93a8c3d2473d5db9fa15eadc6be
Márcio RdeR <rdrgzma@hotmail.com>
2016-09-30 16:02:11 -0300

    Acrescestei inserts no script do banco para testar a funcionalidade

--- Banco de Dados/sprint1_inserts.sql
+++ Banco de Dados/sprint1_inserts.sql
@@ -3,11 +3,31 @@ INSERT INTO `witc`.`tipoperfil`
 VALUES
 ("ESCRITOR");
 
+INSERT INTO `witc`.`tipoperfil`
+(`tipoPerfil`)
+VALUES
+("REVISOR");
+
 
 INSERT INTO `witc`.`tipostatus`
 (`tipoStatus`)
 VALUES
-("PADRAO");
+("EDICAO");
+INSERT INTO `witc`.`tipostatus`
+(`tipoStatus`)
+VALUES
+("REVISAO");
+INSERT INTO `witc`.`tipostatus`
+(`tipoStatus`)
+VALUES
+("FINALIZACAO");
+INSERT INTO `witc`.`tipostatus`
+(`tipoStatus`)
+VALUES
+("PUBLICACAO");
+
+
+
 
 
 INSERT INTO `witc`.`tipoperfil_tem_tipostatus`
@@ -15,5 +35,34 @@ INSERT INTO `witc`.`tipoperfil_tem_tipostatus`
 `TipoStatus_id`)
 VALUES
 (1,1);
+
+INSERT INTO `witc`.`tipogenero`
+(`tipoGenero`)
+VALUES
+("PROSA");
+INSERT INTO `witc`.`tipogenero`
+(`tipoGenero`)
+VALUES
+("CONTO");
+INSERT INTO `witc`.`tipogenero`
+(`tipoGenero`)
+VALUES
+("POESIA");
+
+INSERT INTO `witc`.`tipotexto`
+(`tipoTexto`)
+VALUES
+("TERROR");
+INSERT INTO `witc`.`tipotexto`
+(`tipoTexto`)
+VALUES
+("FICCAO");
+INSERT INTO `witc`.`tipotexto`
+(`tipoTexto`)
+VALUES
+("BIOGRAFIA");
+
+
+
 
 
