4c92b75dff30a6f67df4cf615203f437683d9199
Márcio RdeR <rdrgzma@hotmail.com>
2016-10-03 14:23:31 -0300

    Implmentacao tela revisao livro

--- Implementacao/WritingInTheClouds/src/main/java/br/com/witc/persistencia/HistoricoLivroDAO.java
+++ Implementacao/WritingInTheClouds/src/main/java/br/com/witc/persistencia/HistoricoLivroDAO.java
@@ -1,3 +1,4 @@
+<<<<<<< a9e242ce710a625cb1f3a84919d1c8dac9f1b608
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -83,3 +84,101 @@ public class HistoricoLivroDAO {
                 .executeUpdate();
     }
 }
+=======
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.witc.persistencia;
+
+import br.com.witc.modelo.HistoricoLivro;
+import br.com.witc.modelo.Livro;
+import br.com.witc.modelo.Perfil;
+import br.com.witc.modelo.TipoStatus;
+import static br.com.witc.persistencia.HibernateUtil.getSessionFactory;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.exception.ConstraintViolationException;
+
+/**
+ *
+ * @author 00026108
+ */
+public class HistoricoLivroDAO {
+    Session sessao;
+
+    public HistoricoLivroDAO() {
+        this.sessao = getSessionFactory().getCurrentSession();
+    }
+    
+     public void salvarHistorico(HistoricoLivro hist){
+         
+          try {
+           sessao.saveOrUpdate(hist);
+           
+            } catch (ConstraintViolationException e) {
+          
+                sessao.clear();
+             
+            } 
+        }
+     
+    /**     
+     * @param idLivro O id do livro pesquisado
+     * @return Uma lista com os registros daquele livro no BD
+     */ 
+    public List<HistoricoLivro> listarHistoricoLivro(int idLivro) {
+        return sessao.createQuery("FROM HistoricoLivro WHERE idLivro=:idLivro")
+                .setString("idLivro", String.valueOf(idLivro))
+                .list();
+    }
+    
+    /**
+     * Carrega o histórico de um determinado livro e de um determinado perfil
+     * @param livro O livro buscado
+     * @param perfil O perfil do usuário
+     * @return Um objeto HistoricoLivro
+     */
+    public HistoricoLivro carregarHistoricoLivroUsuario(Livro livro, Perfil perfil) {
+        return (HistoricoLivro) sessao.createCriteria(HistoricoLivro.class)
+                .add(Restrictions.eq("livro", livro))
+                .add(Restrictions.eq("perfil", perfil))
+                .uniqueResult();
+    }
+    
+    /**
+     * Verifica se o livro já foi finalizado pelo usuário
+     * @param idLivro O id do livro buscado
+     * @param idPerfil O id do perfil do usuário
+     * @return Um objeto HistoricoLivro
+     */
+    public boolean estaFinalizadoUsuario(int idLivro, int idPerfil) {
+        HistoricoLivro historico = (HistoricoLivro) sessao.createQuery("FROM HistoricoLivro WHERE idLivro=:idLivro AND idPerfil=:idPerfil")
+                .setString("idLivro", String.valueOf(idLivro))
+                .setString("idPerfil", String.valueOf(idPerfil))
+                .uniqueResult();
+        
+        sessao.refresh(historico);
+        return historico.getDataConclusao() != null;
+    }
+    
+    public HistoricoLivro carregarHistoricoLivroEdicao(Livro livro, TipoStatus status) {
+        return (HistoricoLivro) sessao.createCriteria(HistoricoLivro.class)
+                .add(Restrictions.eq("livro", livro))
+                .add(Restrictions.eq("status", status))
+                .uniqueResult();
+    }
+    
+    public boolean estaFinalizadoEdicao(int idLivro, int idStatus) {
+        HistoricoLivro historico = (HistoricoLivro) sessao.createQuery("FROM HistoricoLivro WHERE idLivro=:idLivro AND idStatus=:idStatus")
+                .setString("idLivro", String.valueOf(idLivro))
+                .setString("idStatus", String.valueOf(idStatus))
+                .uniqueResult();
+        
+        sessao.refresh(historico);
+        return historico.getDataConclusao() != null;
+    }
+}
+>>>>>>> Implmentacao tela revisao livro
--- Implementacao/WritingInTheClouds/src/main/java/br/com/witc/persistencia/LivroDAO.java
+++ Implementacao/WritingInTheClouds/src/main/java/br/com/witc/persistencia/LivroDAO.java
@@ -1,3 +1,4 @@
+<<<<<<< a9e242ce710a625cb1f3a84919d1c8dac9f1b608
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -179,3 +180,172 @@ public class LivroDAO {
                 .uniqueResult();
     }    
 }
+=======
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.witc.persistencia;
+
+import br.com.witc.excessao.BibliotecaVirtualVaziaException;
+import br.com.witc.excessao.LivroException;
+import br.com.witc.modelo.Livro;
+import br.com.witc.modelo.Perfil;
+import br.com.witc.modelo.TipoTexto;
+import static br.com.witc.persistencia.HibernateUtil.getSessionFactory;
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.exception.ConstraintViolationException;
+
+/**
+ *
+ * @author root
+ */
+public class LivroDAO {
+    Session sessao;
+
+    /**
+     *
+     */
+    public LivroDAO() {
+        sessao = getSessionFactory().getCurrentSession();
+    }
+    
+    /**
+     *
+     * @param livro
+     */
+    public void salvarLivro(Livro livro){
+        
+        try {
+           sessao.saveOrUpdate(livro);
+           
+            } catch (ConstraintViolationException e) {
+          
+                sessao.clear();
+             
+            } 
+        }
+    
+    public List<Livro> listarLivrosPerfil(Perfil perfil){
+
+        String consulta ="select livro.* from livro inner join historicolivros on livro.id=historicolivros.idLivro inner join perfil on perfil.id = historicolivros.idPerfil  where idPerfil=:id";
+       List<Livro> lista ;
+        lista= sessao.createSQLQuery(consulta).addEntity("livro",Livro.class).setInteger("id",perfil.getId()).list();
+        return lista;
+       
+
+    }
+
+    /**
+     * Carrega um livro 
+     * @param idLivro O id do livro a ser carregado
+     * @return Livro Um objeto livro
+     * @throws br.com.witc.excessao.LivroException Caso o livro não seja encontrado
+     */
+    public Livro carregarLivro(int idLivro) throws LivroException {        
+        Livro tmpLivro = (Livro) sessao.load(Livro.class, idLivro);
+        if (tmpLivro == null) {
+            throw new LivroException("Nenhum livro encontrado com id " + idLivro);
+        }
+        return tmpLivro;
+    }
+    
+    /**
+     * Lista os livros publicados na Biblioteca Virtual
+     * @param tp O Tipo Texto para pesquisa
+     * @return Uma lista de livros publicados na Biblioteca Virtual
+     * @throws br.com.witc.excessao.BibliotecaVirtualVaziaException Caso não haja livros publicados na Biblioteca
+     */
+    public List<Livro> listarLivrosPorTipoTexto(TipoTexto tp) throws BibliotecaVirtualVaziaException {
+        List<Livro> tmpLstLivro = sessao.createCriteria(Livro.class)
+                .add(Restrictions.like("disponivelBiblioteca", true))
+                .add(Restrictions.eq("tipoTexto", tp))
+                .addOrder(Order.asc("tipoTexto"))
+                .list();
+        
+        if (tmpLstLivro.isEmpty()) {
+            throw new BibliotecaVirtualVaziaException("Nenhum livro foi publicado até o momento.");
+        }
+        return tmpLstLivro;
+    }
+    
+    /**
+     * Lista os livros publicados na Biblioteca Virtual     
+     * @param tp O Tipo Texto para pesquisa
+     * @param campoPesquisa O campo a ser pesquisado
+     * @param valorPesquisa O valor a ser pesquisado
+     * @return Uma lista de livros publicados na Biblioteca Virtual
+     * @throws br.com.witc.excessao.BibliotecaVirtualVaziaException Caso não haja livros publicados na Biblioteca
+     */    
+    public List<Livro> listarLivrosPublicados(TipoTexto tp, String campoPesquisa, String valorPesquisa) 
+            throws BibliotecaVirtualVaziaException {        
+        String sql = "FROM Livro AS l ";
+        switch (campoPesquisa) {
+            case "autor":
+                sql += "INNER JOIN l.tipoTexto AS tp "
+                     + "INNER JOIN l.historicoLivros AS hl "
+                     + "INNER JOIN hl.perfil AS p "
+                     + "INNER JOIN p.usuario AS u "
+                     + "WHERE tp.tipoTexto = '" + tp.getTipoTexto() + "' AND u.nome LIKE ";
+                break;
+            case "classificacao":
+                sql += "INNER JOIN l.tipoTexto tp "
+                     + "WHERE tp.tipoTexto = '" + tp.getTipoTexto() + "' AND l.classificacao LIKE ";
+                break;
+            case "tipoTexto":
+                sql += "INNER JOIN l.tipoTexto tp  "
+                     + "WHERE tp.tipoTexto LIKE ";
+                break;
+            case "titulo":
+                sql += "INNER JOIN l.tipoTexto tp  "
+                     + "WHERE tp.tipoTexto = '" + tp.getTipoTexto() + "' AND l.titulo LIKE ";
+        }
+        sql += "'%" + valorPesquisa + "%' ORDER BY l.tipoTexto";
+                        
+        List<Livro> tmpLstLivro = new ArrayList();
+        List<Object[]> lstResult = sessao.createQuery(sql).list();
+        for (Object[] o : lstResult) {            
+            for (Object o1 : o) {
+                if (o1 instanceof Livro) {
+                    tmpLstLivro.add((Livro) o1);
+                    break;
+                }
+            }
+        }
+                
+        if (tmpLstLivro.isEmpty()) {
+            throw new BibliotecaVirtualVaziaException("Não foi possível localizar livros com os critérios informados.");
+        }
+        return tmpLstLivro;
+    }    
+    
+    /**
+     * Verifica se o livro está disponível para edição do usuário logado
+     * @param idLivro O id do Livro
+     * @param idPerfil O id do perfil do usuário logado
+     * @return True, caso o livro esteja disponível para edição e false, caso contrário
+     */
+    public boolean estaDisponivelEdicaoUsuario(int idLivro, int idPerfil) {
+        Livro livro = (Livro) sessao.createQuery("FROM Livro WHERE id=:idLivro")
+                .setString("idLivro", String.valueOf(idLivro))                
+                .uniqueResult();
+        
+        sessao.refresh(livro);
+        return (livro.getBookLock() == idPerfil) || (livro.getBookLock() == 0);
+    }
+    
+    public boolean estaDisponivelRevisaoUsuario(int idLivro, int idPerfil) {
+        Livro livro = (Livro) sessao.createQuery("FROM Livro WHERE id=:idLivro")
+                .setString("idLivro", String.valueOf(idLivro))                
+                .uniqueResult();
+        
+        sessao.refresh(livro);
+        return (livro.getBookLock() == idPerfil) || (livro.getBookLock() == 0);
+    }
+}
+>>>>>>> Implmentacao tela revisao livro
