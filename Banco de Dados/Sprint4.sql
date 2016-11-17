54671d5ea595e1f2c2375996a23414956e390262
Cassiane <cassianeribasdasilva@gmail.com>
2016-11-17 19:45:53 -0200

    Tabela de notificacoes

--- Banco de Dados/Sprint4.sql
+++ Banco de Dados/Sprint4.sql
@@ -1,7 +1,6 @@
 CREATE TABLE `HistoriasDesafios` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `idTipoTexto` int(10) unsigned NOT NULL,
-  `idDesafiosPalavras` int(10) unsigned NOT NULL,
   `idDesafiosUsuarios` int(10) unsigned NOT NULL,
   `classificacao` varchar(45) NOT NULL,
   `disponivelBiblioteca` tinyint(1) NOT NULL,
@@ -12,9 +11,21 @@ CREATE TABLE `HistoriasDesafios` (
   `texto` longtext NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_HistoriaDesafio_TipoTextoDesafios_idx` (`idTipoTexto`),
-  KEY `fk_HistoriaDesafio_DesafiosPalavras_idx` (`idDesafiosPalavras`),
   KEY `fk_HistoriaDesafio_DesafiosUsuarios_idx` (`idDesafiosUsuarios`),
   CONSTRAINT `fk_HistoriaDesafio_TipoTextoDesafios` FOREIGN KEY (`idTipoTexto`) REFERENCES `tipotexto` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_HistoriaDesafio_DesafiosPalavras` FOREIGN KEY (`idDesafiosPalavras`) REFERENCES `desafiospalavras` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_HistoriaDesafio_DesafiosUsuarios` FOREIGN KEY (`idDesafiosPalavras`) REFERENCES `desafiosusuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_HistoriaDesafio_DesafiosUsuarios` FOREIGN KEY (`idDesafiosUsuarios`) REFERENCES `desafiosusuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+
+use witc;
+CREATE TABLE `Notificacoes` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `texto` longtext NOT NULL,
+  `idUsuarioRemetente` int unsigned NOT NULL,
+  `idUsuarioDestinatario` int unsigned NOT NULL,
+  `idDesafiosUsuarios` int unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_Notificacoes_idUsuarioRemetente` FOREIGN KEY (`idUsuarioRemetente`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Notificacoes_idUsuarioDestinatario` FOREIGN KEY (`idUsuarioDestinatario`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Notificacoes_idDesafiosUsuarios` FOREIGN KEY (`idDesafiosUsuarios`) REFERENCES `desafiosusuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
